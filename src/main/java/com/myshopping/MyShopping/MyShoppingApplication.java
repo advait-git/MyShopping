6f2603a3d47b9d36d6e996a7c4b91126ef359c99
advait-git <advaittiwari2@gmail.com>
2024-03-28 14:56:40 +0530

    app user and db

--- src/main/java/com/myshopping/MyShopping/MyShoppingApplication.java
+++ src/main/java/com/myshopping/MyShopping/MyShoppingApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyShoppingApplication {
+public class
+MyShoppingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyShoppingApplication.class, args);
